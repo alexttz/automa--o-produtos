1294da035d8fd4daf5f12a0255a5a20a5c9b5789
Alexander Lima <lima.alexander.las@gmail.com>
2024-03-11 14:18:48 -0300

    Finalizing the code, added a conditional for the ‘obs’ field in the table and optimized form navigation.

--- codigo.py
+++ codigo.py
@@ -6,10 +6,10 @@ link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
 pyautogui.PAUSE = 0.5
 
-#Abrir o navegador Chrome:
+#Abrir o navegador Chrome:1
 pyautogui.press("win")
 pyautogui.write("chrome")
-pyautogui.press("enter")
+pyautogui.press("enter")   
 
 pyautogui.write(link)
 pyautogui.press("enter")
@@ -18,11 +18,12 @@ pyautogui.press("enter")
 time.sleep(3)
 
 #Realizar login (email e senha)
-pyautogui.click(x=1043, y=405)
+pyautogui.press("tab")
 pyautogui.write("automatizadorpython@email.com")
 pyautogui.press("tab")
 pyautogui.write("123456789@")
-pyautogui.click(x=1274, y=568)
+pyautogui.press("tab")
+pyautogui.press("enter")
 
 #Pausa para carregamento
 time.sleep(3)
@@ -55,9 +56,11 @@ for linha in tabela.index:
     pyautogui.write(str(tabela.loc[linha, "custo"]))
     pyautogui.press("tab")
 
-    pyautogui.write(str(tabela.loc[linha, "obs"]))
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
 
-    pyautogui.click(x=1196, y=767)
-
+    pyautogui.press("tab")
+    pyautogui.press("enter")
     pyautogui.scroll(5000)
 
